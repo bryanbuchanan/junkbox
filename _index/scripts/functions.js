c96d0513b01ef1445c72e065d0a0424c67f4209d
Bryan Buchanan <bryan@resen.co>
2012-12-04 22:36:46 -0800

    Fixed index folder links in javascript

--- _index/scripts/functions.js
+++ _index/scripts/functions.js
@@ -32,25 +32,25 @@
 	function icons() {
 	
 		// Images
-		$('a[href$="pdf"] img').attr('src', home_uri + '/_index/images/icons/icon-image.png');
-		$('a[href$="ai"] img').attr('src', home_uri + '/_index/images/icons/icon-ai.png');
-		$('a[href$="eps"] img').attr('src', home_uri + '/_index/images/icons/icon-image.png');
-		$('a[href$="tif"] img').attr('src', home_uri + '/_index/images/icons/icon-image.png');
-		$('a[href$="bmp"] img').attr('src', home_uri + '/_index/images/icons/icon-image.png');
+		$('a[href$="pdf"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-image.png');
+		$('a[href$="ai"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-ai.png');
+		$('a[href$="eps"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-image.png');
+		$('a[href$="tif"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-image.png');
+		$('a[href$="bmp"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-image.png');
 	
 		// Videos
-		$('a[href$="fla"] img').attr('src', home_uri + '/_index/images/icons/icon-fla.png');
-		$('a[href$="flv"] img').attr('src', home_uri + '/_index/images/icons/icon-flv.png');
-		$('a[href$="swf"] img').attr('src', home_uri + '/_index/images/icons/icon-swf.png');
-		$('a[href$="mov"] img').attr('src', home_uri + '/_index/images/icons/icon-video.png');
-		$('a[href$="mpg"] img').attr('src', home_uri + '/_index/images/icons/icon-video.png');
+		$('a[href$="fla"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-fla.png');
+		$('a[href$="flv"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-flv.png');
+		$('a[href$="swf"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-swf.png');
+		$('a[href$="mov"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-video.png');
+		$('a[href$="mpg"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-video.png');
 	
 		// Documents
-		$('a[href$="doc"] img, a[href$="docx"] img').attr('src', home_uri + '/_index/images/icons/icon-doc.png');
-		$('a[href$="xls"] img, a[href$="xlsx"] img').attr('src', home_uri + '/_index/images/icons/icon-xls.png');
+		$('a[href$="doc"] img, a[href$="docx"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-doc.png');
+		$('a[href$="xls"] img, a[href$="xlsx"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-xls.png');
 		
 		// Archives
-		$('a[href$="zip"] img').attr('src', home_uri + '/_index/images/icons/icon-zip.png');
+		$('a[href$="zip"] img').attr('src', home_uri + '/' + index_folder + '/images/icons/icon-zip.png');
 
 	}
 	
