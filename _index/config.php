8577e68b6b88732dad380a763bcbc2af7cc63722
Bryan Buchanan <bryan@resen.co>
2012-12-04 21:55:56 -0800

    Disabled admin accounts for now

--- _index/config.php
+++ _index/config.php
@@ -8,9 +8,9 @@ $order = "asc"; // asc or desc
 $index_folder = "_index"; // Name of the index folder
 
 // Viewer Accounts
-$viewer[] = array('name', 'password');
+// $viewer[] = array('name', 'password');
 
 // Admin Accounts (not yet working)
-$admin[] = array('admin', 'admin');
+// $admin[] = array('admin', 'admin');
 
 ?>
