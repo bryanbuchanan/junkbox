a3543120858372ffe0db9d4a236a2951646c1f5e
Bryan Buchanan <bryan@resen.co>
2012-12-10 10:39:49 -0800

    Added list of disallowed file types for upload

--- _index/config.php
+++ _index/config.php
@@ -6,6 +6,7 @@ $search_engine_indexing = false; // allow search engines to index your folders/f
 $sort = "name"; // name or date
 $order = "asc"; // asc or desc
 $index_folder = "_index"; // Name of the index folder
+$disallowed_file_types = "php,sh";
 
 // Viewer Accounts
 // $viewer[] = array('name', 'password');
